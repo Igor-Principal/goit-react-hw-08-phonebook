c2cdbc86e3e665e95622e145b5792f0780439963
Igor-Principal <igorprincipal@gmail.com>
2024-02-13 19:11:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { authSelector } from 'store/Auth/authSelector';
+import { getProfileThunk } from 'store/Auth/auth-thunk';
+import { setToken } from 'services/auth-service';
+
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 import PublicRoute from './PublicRoute/PublicRoute';
-
-// const Layout = lazy(() => import('./Layout/Layout'));
-// const ContactsPart = lazy(() => import('../Pages/ContactsPart/ContactsPart'));
-// const StartPage = lazy(() => import('../Pages/StartPage/StartPage'));
-// const Registration = lazy(() => import('../Pages/Registration/Registration'));
-// const Login = lazy(() => import('../Pages/Login/Login'));
 import Layout from './Layout/Layout';
 import ContactsPart from 'Pages/ContactsPart/ContactsPart';
 import StartPage from 'Pages/StartPage/StartPage';
 import Registration from '../Pages/Registration/Registration';
 import Login from 'Pages/Login/Login';
 import ErrorPage from 'Pages/ErrorPage/ErrorPage';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { authSelector } from 'store/Auth/authSelector';
-import { getProfileThunk } from 'store/Auth/auth-thunk';
-import { setToken } from 'services/auth-service';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -26,7 +21,7 @@ export const App = () => {
 
   useEffect(() => {
     if (token) {
-       setToken(`Bearer ${token}`);
+      setToken(`Bearer ${token}`);
       dispatch(getProfileThunk());
     } else {
       return;
