4302baa84bb30f593c3b2e828dfc10a277ff2f75
Igor-Principal <igorprincipal@gmail.com>
2024-02-13 19:08:17 +0200

    update

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
   const auth = useSelector(state => state.auth.token);
-  // const { isLoading } = useSelector(state => state.auth);
   const location = useLocation();
 
   return auth ? children : <Navigate to={'/login'} state={location} />;
